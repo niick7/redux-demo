1f0355853b05086b70825289d06d550e6a333ecd
Nhan Vo <niick7@gmail.com>
2024-11-21 15:40:04 -0600

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import { createStore } from "redux";
+
+const initialState = {
+  counter: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
