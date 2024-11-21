5479bebafd14393017d0414967c669f53388d298
Nhan Vo <niick7@gmail.com>
2024-11-21 16:30:15 -0600

    Manage multiple state with Redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,10 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatch = useDispatch();
   // We assign state to redux to manage
   // Now we get it from redux and only get a part of state, that is a counter
   const counter = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
+  const show = useSelector((state) => state.showCounter);
 
   const increamentHandler = () => {
     // dispatch will send an action to the redux
@@ -25,16 +26,20 @@ const Counter = () => {
   const decreamentHandler = () => {
     dispatch({ type: "decrement" });
   };
+  function toggleCounterHandler() {
+    dispatch({ type: "toggle" });
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={increamentHandler}>Increment</button>
         <button onClick={increaseHandler}>Increment by 5</button>
         <button onClick={decreamentHandler}>Decrement</button>
       </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
