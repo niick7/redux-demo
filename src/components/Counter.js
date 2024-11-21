5c29e2bd8c75770306589bca1c4921a6a052e729
Nhan Vo <niick7@gmail.com>
2024-11-21 15:53:04 -0600

    Link compoment to the Reduct store

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,17 @@
-import classes from './Counter.module.css';
+import { useSelector } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  // We assign state to redux to manage
+  // Now we get it from redux and only get a part of state, that is a counter
+  const counter = useSelector((state) => state.counter);
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
